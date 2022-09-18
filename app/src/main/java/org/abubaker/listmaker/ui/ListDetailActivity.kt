da3f59b6ddb7ffb1f84e17b8c24fd7613ced559f
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-09-18 16:56:27 +0500

    Creating ListDetailActivity.kt

---
+++ app/src/main/java/org/abubaker/listmaker/ui/ListDetailActivity.kt
@@ -0,0 +1,14 @@
+package org.abubaker.listmaker.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.abubaker.listmaker.R
+
+class ListDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_detail)
+    }
+
+}
